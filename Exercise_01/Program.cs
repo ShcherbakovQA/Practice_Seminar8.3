b90ebd90fd3b1d72b7270602d7744b93566a90a8
Alexander <alex17.05.84@yandex.ru>
2022-11-06 13:29:45 +0300

    Exercise 01 done

---
+++ Exercise_01/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача №. Написать рекурсивный метод для деления одного числа на другое. 
+// Нужен остаток и результат деления. 
+
+int InputNumber(string text)  // ручной ввод числа
+{
+    Console.Write(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int DivisionRemainder(int number, int divider)
+{
+    if (number >= divider)
+    {
+        number -= divider;
+        return DivisionRemainder(number, divider);
+    }
+    else return number;
+}
+
+int DivisionFull(int number, int divider)
+{
+     if (number >= divider)
+    {
+        number -= divider;
+        return DivisionFull(number, divider) + 1;
+    }
+    else return 0;
+}
+
+int number = InputNumber("Введите числитель: ");
+int divider = InputNumber("Введите знаменатель: ");
+
+Console.WriteLine($"{number}/{divider}={DivisionFull(number,divider)},остаток={DivisionRemainder(number,divider)}");
+
